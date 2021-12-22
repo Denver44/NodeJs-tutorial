c14a0e345406a575249903074a6fb668a1ea9746
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-22 09:34:24 +0530

    OS Module of Node

--- 1.NodeJs/8.OSModule/index.js
+++ 1.NodeJs/8.OSModule/index.js
@@ -1,6 +1,5 @@
-const os = require("os");
+const os = require("os"); // With this we can get all information of our operating system.
 
-// With this we can get all information of our operating system.
 console.log(os.arch()); // this tell which architecture we are using.
 console.log(os.freemem() / (1024 * 1024 * 1024)); //  this tell total ram memory free
 console.log(os.totalmem() / (1024 * 1024 * 1024)); // this tell total ram memory
