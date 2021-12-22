89b683a91c5cdf1115d98c6c37d1d6ca1353256d
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-22 09:37:19 +0530

    Event Module

--- 1.NodeJs/10EventModule/index.js
+++ 1.NodeJs/10EventModule/index.js
@@ -1,11 +1,5 @@
-// Event Module
-// Node.js has a built-in Module called "Events"
-// Where we can Create-, fire and listen for our own events.
-
-const EvenEmitter = require("events");
+const EvenEmitter = require("events"); // Event Module Node.js has a built-in Module called "Events" Where we can Create-, fire and listen for our own events.
 const event = new EvenEmitter(); // now we have create a object now we can use all function.
-
-
 
 
 
