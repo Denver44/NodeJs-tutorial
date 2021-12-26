06eb2ef068cc4bfc5334f3193fd922c26076b679
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-26 12:25:23 +0530

    Call back hell

--- 13AsynchronousJS/1.CallBack-Hell/index.js
+++ 13AsynchronousJS/1.CallBack-Hell/index.js
@@ -2,20 +2,17 @@ const fs = require('fs');
 const axios = require('axios');
 
 fs.readFile('./dog.txt', 'utf-8', (error, data) => {
-  try {
-    console.log('Breed : ', data);
-    axios
-      .get(`https://dog.ceo/api/breed/${data}/images/random`)
-      .then((res) => {
-        fs.writeFile('dog-img-url.txt', res.data.message, (err) => {
-          if (err) return console.log('writeFile Error ', err.message);
-          else console.log('Random dog imag URL added to the file');
-        });
-      })
-      .catch((error) => {
-        console.log('API call error ', error.message);
+  if (error) return console.log('readFile Error', error);
+  console.log('Breed : ', data);
+  const response = axios.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  response
+    .then((res) => {
+      fs.writeFile('dog-img-url.txt', res.data.message, (err) => {
+        if (err) return console.log('writeFile Error ', err.message);
+        else console.log('Random dog imag URL added to the file');
       });
-  } catch (error) {
-    console.log(error.message);
-  }
+    })
+    .catch((error) => {
+      console.log('API call error ', error.message);
+    });
 });
